40cabee4732ccf7176c56bc67196875f61ce6578
MyPhuongKhong <myphuongkhong@gmail.com>
2025-04-22 18:17:11 +0700

    Fix mongoose

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to database!')
     app.listen(process.env.PORT, () => {
-      console.log('Server is running on port 3000')
+      console.log('Server is running on port http://localhost:3000')
     })
   })
   .catch((error) => {
